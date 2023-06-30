3d2cade329186137c350ab4a74fbcbb5dc1796b0
aanos-m <aanosmahmood@yahoo.com>
2023-06-29 23:27:14 -0500

    initialized HeroSection

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const HeroSection = (props: Props) => {
+  return (
+    <div className='p-5 m-5 bg-slate-200'>HeroSection</div>
+  )
+}
+
+export default HeroSection
