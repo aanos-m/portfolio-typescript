635ea001a9bf6936d30ede2b56b67144a2ce50e7
aanos-m <aanosmahmood@yahoo.com>
2023-06-30 21:44:30 -0500

    created expereince/work section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Experience/>
     </div>
   );
 }
