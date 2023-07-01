1bb5b8d203280961bd6b925dee817546a854af99
aanos-m <aanosmahmood@yahoo.com>
2023-06-30 21:58:44 -0500

    initialized projects-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Experience/>
+      <Projects/>
     </div>
   );
 }
